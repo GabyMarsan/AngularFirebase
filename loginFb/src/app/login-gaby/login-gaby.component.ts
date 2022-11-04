f4fe36511e4f2f0a26e192332b86d97bbaa69df0
GabyMarsan <anagbms07@gmail.com>
2022-11-04 22:05:42 +0000

    login part 2

--- loginFb/src/app/login-gaby/login-gaby.component.ts
+++ loginFb/src/app/login-gaby/login-gaby.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-gaby',
@@ -8,19 +8,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class LoginGabyComponent implements OnInit {
 
-  loginForm?: FormGroup;
-  constructor() { }
+  hide: boolean = false;
+
+  constructor(private fb: FormBuilder) { 
+
+  }
 
   ngOnInit(){
-    this.loginForm = new FormGroup(
-      {
-        email : new FormControl('',[Validators.required, Validators.email]),
-        password: new FormControl('',[Validators.required, Validators.minLength(6)])
-      }
-    );
   }
-  onLogin(){
 
+  loginForm: FormGroup = this.fb.group({
+    email: ['',[Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+
+  onLogin(){
+    if (!this.loginForm.valid){
+      return;
+    }
+    console.log(this.loginForm.value);
   }
 
 }
